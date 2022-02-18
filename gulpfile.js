3c8a954c5a67064b39459c7810ef48710eb8b618
vijaysolaraju369 <97930736+vijaysolaraju369@users.noreply.github.com>
2022-02-18 15:57:53 +0530

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-
-gulp.task('sass', function(){
-    return gulp.src('scss/styles.scss')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass
-      .pipe(gulp.dest('css'))
-  });
